160684ba78e29f4de68595ddb92ff55248ba51aa
李喆 <lizhe1@wisetv.com.cn>
2015-05-18 22:55:21 +0800

    Initial Commit

---
+++ opencvtest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencvtest
+//
+//  Created by mac on 15/5/18.
+//  Copyright (c) 2015年 TJGD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
